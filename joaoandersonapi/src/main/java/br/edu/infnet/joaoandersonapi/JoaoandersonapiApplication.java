b7456bcdad58511f74222f0e188175c6fc8a6260
João Anderson <joao.anderson.af@gmail.com>
2025-07-29 21:10:24 -0300

    Criando projeto Spring Boot

---
+++ joaoandersonapi/src/main/java/br/edu/infnet/joaoandersonapi/JoaoandersonapiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.joaoandersonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoaoandersonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoaoandersonapiApplication.class, args);
+	}
+
+}
---
+++ joaoandersonapi/src/test/java/br/edu/infnet/joaoandersonapi/JoaoandersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.joaoandersonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoaoandersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
