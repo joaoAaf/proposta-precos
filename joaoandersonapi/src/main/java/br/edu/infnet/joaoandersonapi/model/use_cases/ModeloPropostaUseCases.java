c5d9c796c361c0aef3b8b7d383c0b52c357e604a
João Anderson <joao.anderson.af@gmail.com>
2025-08-03 19:41:25 -0300

    Criando as interfaces ComumUseCases e ModeloPropostaUseCases.

---
+++ joaoandersonapi/src/main/java/br/edu/infnet/joaoandersonapi/model/use_cases/ModeloPropostaUseCases.java
@@ -0,0 +1,7 @@
+package br.edu.infnet.joaoandersonapi.model.use_cases;
+
+import br.edu.infnet.joaoandersonapi.model.dtos.ModeloPropostaDto;
+
+public interface ModeloPropostaUseCases extends ComumUseCases<ModeloPropostaDto, Long> {
+
+}
