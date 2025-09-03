8c980106e0f067f578454bcb2146e2829a108824
João Anderson <joao.anderson.af@gmail.com>
2025-09-03 14:28:35 -0300

    Criando api para relatórios

---
+++ joaoandersonrelatoriosapi/src/main/java/br/edu/infnet/joaoandersonrelatoriosapi/JoaoandersonrelatoriosapiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.joaoandersonrelatoriosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoaoandersonrelatoriosapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoaoandersonrelatoriosapiApplication.class, args);
+	}
+
+}
