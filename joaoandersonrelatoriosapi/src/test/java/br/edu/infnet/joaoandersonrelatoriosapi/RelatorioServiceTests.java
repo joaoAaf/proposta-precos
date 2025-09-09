0da5a53dd11f6e520e1a28c35fdfaa632276a006
João Anderson <joao.anderson.af@gmail.com>
2025-09-09 13:04:35 -0300

    Criando testes para o metodo gerarRelatorioComparacaoPropostas

---
+++ joaoandersonrelatoriosapi/src/test/java/br/edu/infnet/joaoandersonrelatoriosapi/RelatorioServiceTests.java
@@ -0,0 +1,83 @@
+package br.edu.infnet.joaoandersonrelatoriosapi;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import br.edu.infnet.joaoandersonrelatoriosapi.model.domain.Material;
+import br.edu.infnet.joaoandersonrelatoriosapi.model.domain.Proposta;
+import br.edu.infnet.joaoandersonrelatoriosapi.model.service.RelatorioService;
+
+public class RelatorioServiceTests {
+
+    private final RelatorioService relatorioService;
+
+    private Proposta proposta1;
+    private Proposta proposta2;
+    private Proposta proposta3;
+
+    public RelatorioServiceTests() {
+        this.relatorioService = new RelatorioService();
+    }
+
+    @BeforeEach
+    void setUp() {
+        var material1 = new Material(1, 55.0, 12.0, 660.0);
+        var material2 = new Material(2, 2.20, 104.6, 230.12);
+
+        var material3 = new Material(1, 55.0, 10.0, 550.0);
+        var material4 = new Material(2, 2.20, 100.5, 220.1);
+
+        var material5 = new Material(1, 55.0, 8.0, 440.0);
+        var material6 = new Material(2, 2.20, 98.5, 216.7);
+
+        this.proposta1 = new Proposta(Long.valueOf(1), Arrays.asList(material1, material2), 0.0,
+                BigDecimal.valueOf(890.12));
+        this.proposta2 = new Proposta(Long.valueOf(2), Arrays.asList(material3, material4), 0.0,
+                BigDecimal.valueOf(771.1));
+        this.proposta3 = new Proposta(Long.valueOf(3), Arrays.asList(material5, material6), 0.0,
+                BigDecimal.valueOf(656.7));
+    }
+
+    @DisplayName("Deve gerar relatório de comparação de propostas quando houver duas propostas")
+    @Test
+    public void deveGerarRelatorioComparacaoPropostas_quandoHouverDuasPropostas() {
+        // Dados
+        var propostas = Arrays.asList(proposta1, proposta2);
+        var mediaMediana = BigDecimal.valueOf(830.61).setScale(2, RoundingMode.HALF_EVEN);
+        var desvioPadraoPercentual = BigDecimal.valueOf(7.16).setScale(2, RoundingMode.HALF_EVEN);
+        var propostasVantajosas = Arrays.asList(proposta2);
+        // Quando
+        var relatorioComparacao = relatorioService.gerarRelatorioComparacaoPropostas(propostas);
+        // Então
+        assertEquals(mediaMediana, relatorioComparacao.getMedia());
+        assertEquals(mediaMediana, relatorioComparacao.getMediana());
+        assertEquals(desvioPadraoPercentual, relatorioComparacao.getDesvioPadraoPercentual());
+        assertEquals(propostasVantajosas, relatorioComparacao.getPropostasVantajosas());
+    }
+
+    @DisplayName("Deve gerar relatório de comparação de propostas quando houver mais de duas propostas")
+    @Test
+    public void deveGerarRelatorioComparacaoPropostas_quandoHouverMaisDeDuasPropostas() {
+        // Dados
+        var propostas = Arrays.asList(proposta1, proposta2, proposta3);
+        var media = BigDecimal.valueOf(772.64).setScale(2, RoundingMode.HALF_EVEN);
+        var mediana = BigDecimal.valueOf(771.1).setScale(2, RoundingMode.HALF_EVEN);
+        var desvioPadraoPercentual = BigDecimal.valueOf(12.33).setScale(2, RoundingMode.HALF_EVEN);
+        var propostasVantajosas = Arrays.asList(proposta3);
+        // Quando
+        var relatorioComparacao = relatorioService.gerarRelatorioComparacaoPropostas(propostas);
+        // Então
+        assertEquals(media, relatorioComparacao.getMedia());
+        assertEquals(mediana, relatorioComparacao.getMediana());
+        assertEquals(desvioPadraoPercentual, relatorioComparacao.getDesvioPadraoPercentual());
+        assertEquals(propostasVantajosas, relatorioComparacao.getPropostasVantajosas());
+    }
+
+}
