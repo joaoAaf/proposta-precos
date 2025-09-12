f6dc8d930b29772863f57f0ca37c025ef65010d7
João Anderson <joao.anderson.af@gmail.com>
2025-09-12 16:14:01 -0300

    Criando a classe RelatorioController com a rota `/api/relatorio` e um metodo GET para gerar o relatorio de comparação.

---
+++ joaoandersonrelatoriosapi/src/main/java/br/edu/infnet/joaoandersonrelatoriosapi/controller/RelatorioController.java
@@ -0,0 +1,29 @@
+package br.edu.infnet.joaoandersonrelatoriosapi.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.infnet.joaoandersonrelatoriosapi.model.domain.Proposta;
+import br.edu.infnet.joaoandersonrelatoriosapi.use_cases.RelatorioUseCases;
+
+@RestController
+@RequestMapping("/api/relatorio")
+public class RelatorioController {
+
+    private final RelatorioUseCases relatorioUseCases;
+
+    public RelatorioController(RelatorioUseCases relatorioUseCases) {
+        this.relatorioUseCases = relatorioUseCases;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> gerarRelatorioComparacaoPropostas(List<Proposta> propostas) {
+        var relatorio = this.relatorioUseCases.gerarRelatorioComparacaoPropostas(propostas);
+        return ResponseEntity.ok(relatorio);
+    }
+
+}
