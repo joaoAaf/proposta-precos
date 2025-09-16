4482ff3ec401a2e20925078de3ba4e9a990a9314
João Anderson <joao.anderson.af@gmail.com>
2025-09-16 10:23:23 -0300

    Criando projeto para iniciar refatoração

---
+++ joaoanderson-apis/proposta-precos/src/main/java/br/com/apisemaperreio/proposta_precos/PropostaPrecosApplication.java
@@ -0,0 +1,13 @@
+package br.com.apisemaperreio.proposta_precos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropostaPrecosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropostaPrecosApplication.class, args);
+	}
+
+}
