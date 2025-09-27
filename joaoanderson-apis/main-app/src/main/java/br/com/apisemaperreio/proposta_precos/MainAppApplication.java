db3842f72f524c7c4ed71c2dbe43986df8b0527a
João Anderson <joao.anderson.af@gmail.com>
2025-09-27 10:37:33 -0300

    Movendo a anotação EnableFeignClients do modulo external-api para o main-app

--- joaoanderson-apis/main-app/src/main/java/br/com/apisemaperreio/proposta_precos/MainAppApplication.java
+++ joaoanderson-apis/main-app/src/main/java/br/com/apisemaperreio/proposta_precos/MainAppApplication.java
@@ -2,8 +2,10 @@ package br.com.apisemaperreio.proposta_precos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MainAppApplication {
 
 	public static void main(String[] args) {
